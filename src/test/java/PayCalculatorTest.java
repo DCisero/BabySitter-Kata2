6adb02e93ebea9b3bbcc80879c228fda9d126cac
DeShayla Cisero <deshayla.cisero@integral.io>
2019-04-11 14:47:47 -0400

    PayCalculator calculates total cost for FamilyB and FamilyC

--- src/test/java/PayCalculatorTest.java
+++ src/test/java/PayCalculatorTest.java
@@ -12,9 +12,23 @@ public class PayCalculatorTest {
     }
 
     @Test
-    public void familyA_works5to4_payIs190() {
+    public void familyA_works5To4_payIs190() {
         PayCalculator payCalculator = new PayCalculator(new FamilyA());
 
         assertEquals(190, payCalculator.calculate(Hour.fivePM(), Hour.fourAM()));
+    }
+
+    @Test
+    public void familyB_works5To4_payIs140() {
+        PayCalculator payCalculator = new PayCalculator(new FamilyB());
+
+        assertEquals(140, payCalculator.calculate(Hour.fivePM(), Hour.fourAM()));
+    }
+
+    @Test
+    public void familyC_works5To4_payIs189() {
+        PayCalculator payCalculator = new PayCalculator(new FamilyC());
+
+        assertEquals(189, payCalculator.calculate(Hour.fivePM(), Hour.fourAM()));
     }
 }
